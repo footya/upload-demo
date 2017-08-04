184395605b6e4326940613fb28d6ec6e8dcf693c
footya <footya@gmail.com>
2017-08-04 14:33:13 +0800

    express upload server and form submit

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import multer from 'multer';  // for parsing multipart/form-data
+import bodyParser from 'body-parser';
+import fs from 'fs';
+const app = express();
+// 设定dest才真的上传
+const upload = multer({dest: 'upload/'});
+app.use(bodyParser.json());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+
+});
+app.all('/upload', upload.single('filename'), (req, res, next)=>{
+    const tmp_path = req.file.path;
+    console.log(req.file);
+    const target_path = 'uploads/' + req.file.originalname;
+    const src = fs.createReadStream(tmp_path);
+    const dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
+    src.on('end', function(){
+        res.send('done'+ '<img src="' + target_path + '"/>');
+    });
+    src.on('error', function(){
+        res.send('error');
+    });
+    console.log(tmp_path);
+});
+app.get('/form', (req, res) => {
+    res.sendFile(__dirname + '/form.html');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+
+});
