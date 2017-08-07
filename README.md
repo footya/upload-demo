f21900bb0c27879769e1d97db64d80fb4e293662
footya <footya@gmail.com>
2017-08-07 10:22:43 +0800

    add progress percent

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # upload-demo
 upload demo for web
 ## start
-```
-npm install
-npm start
-```
+```npm install```
 
-简单form表单图片上传：http:127.0.0.1:3000/form
+简单form表单文件上传：http:127.0.0.1:3000/form
+```npm run start```
+
+rc-upload 带进度文件上传: http://127.0.0.1:3001
+```npm run react```
 
 ## 参考链接
 - [Handle File Uploads in Express (Node.js)](http://www.hacksparrow.com/handle-file-uploads-in-express-node-js.html)
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import './app.css';
 export default class App extends Component{
 	constructor(props){
 		super(props);
+		let _this = this;
 		this.uploaderProps = {
 	      action: '/upload',
 	      data: { a: 1, b: 2 },
@@ -22,6 +23,9 @@ export default class App extends Component{
 	        console.log('onSuccess', file);
 	      },
 	      onProgress(step, file) {
+	      	_this.setState({
+	      		percent: step.percent
+	      	});
 	        console.log('onProgress', Math.round(step.percent), file.name);
 	      },
 	      onError(err) {
@@ -30,12 +34,14 @@ export default class App extends Component{
 	    };
 	    this.state = {
 	      destroyed: false,
+	      percent: 0
 	    };
 	}
     render() {
         return (
             <div className="app">
                 <h2>测试上传</h2>
+                <p>上传进度：{this.state.percent}%</p>
                 <Upload {...this.uploaderProps}>开始上传</Upload>
             </div>
         );
